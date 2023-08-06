2fe42f261ab748628642741dd9b47d17c9abe2eb
ccb-github <photonvar@protonmail.com>
2023-08-06 08:34:10 +0800

    Allowedcategory misspelling fix

--- app/components/common/AllowedCatgory.tsx
+++ app/components/common/AllowedCatgory.tsx
@@ -2,15 +2,15 @@ import { useContext, useEffect, useRef } from "react"
 import { AppContext } from "../AppProvider"
 import { SchemaResultMapper } from "#/types/schema"
 
-export const AllowedCatgoryList = ({list}: {list: string[]}) => {
+export const AllowedCategoryList = ({list}: {list: string[]}) => {
   const appContext = useContext(AppContext)
   const { useCollection } = appContext
-  const targetCollection = useCollection("Catgory")
-  const allCatgorysRef = useRef<any[]>([])
+  const targetCollection = useCollection("Category")
+  const allCategorysRef = useRef<any[]>([])
   useEffect( () => {
     (async () => {
       const catgorys = await targetCollection?.find()
-      catgorys ? allCatgorysRef.current = catgorys.map( item => item.name) : null 
+      catgorys ? allCategorysRef.current = catgorys.map( item => item.name) : null 
     })()
     
   }, [targetCollection])
@@ -19,7 +19,7 @@ export const AllowedCatgoryList = ({list}: {list: string[]}) => {
     <tr>
       <th scope="row">Catgory</th>
       {
-        allCatgorysRef.current.map(
+        allCategorysRef.current.map(
           catgory => <td><b>{catgory}</b></td>
         )
       }
